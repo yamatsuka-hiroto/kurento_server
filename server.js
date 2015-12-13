97a4b8623b4dce53e00b8b3201c2ee3e35799790
yamatsuka_hiroto <yamatsuka_hiroto@yamatsuka-hiroto-no-MacBook-Pro.local>
2015-12-13 20:06:50 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+var kurento = require("kurento-client");
+
+var kurentoClient = null;
+const ws_uri = "ws://104.154.55.190:8888/kurento";
+
+function prepaereKurento() {
+    kurento(ws_uri, function (error, client) {
+        if (error) {
+            return error;
+        }
+
+        kurentoClient = client;
+    });
+}
+
+prepaereKurento();
+
+
+// -- create the cokcet server on the port
+var srv = require("http").Server();
+var io = require("socket.io")(srv);
+var port = 8000;
+srv.listen(port);
+io.on("connection", function (socket) {
+    socket.on("echoback_request", function (sdp) {
+        handleEchobackOffer(sdp, socket);
+    });
+
+});
+
+var media_pipeline = null;
+function handleEchobackOffer(sdp, socket){
+    kurentoClient.create("MediaPipeline",function(error,pipeline){
+        if(error){
+            onError(error);
+            return null;
+        }
+        media_pipeline = pipeline;
+
+        pipeline.create("WebRtcEndpoint",function(error,webRtcEndpoint){
+            webRtcEndpoint.connect(webRtcEndpoint,function(error){
+                webRtcEndpoint.processOffer(sdp,function(error,sdpAnswer){
+                    socket.emit("echoback_response",{sdp:sdpAnswer});
+                });
+            });
+        });
+    });
+}
